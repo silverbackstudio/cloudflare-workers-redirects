798ce843795a1edb20afec98c786ef4c0900368b
Brando Meniconi <b.meniconi@silverbackstudio.it>
2019-11-04 10:31:10 +0100

    extract port and addr as CONST in the api-proxy

--- src/api-proxy.js
+++ src/api-proxy.js
@@ -1,12 +1,14 @@
 var http = require('http')
 var https = require('https')
 
+const PROXY_PORT = 8010;
+const PROXY_ADDRESS = '127.0.0.1';
+
 const proxy = http.createServer( function(request, response){
     
     console.log( '%s %s', request.method, request.url);
 
     if ( request.method === 'OPTIONS' ) {
-        console.log('CORS preflight request');
         response.writeHead(200, { 
             'access-control-allow-origin': '*',
             'access-control-allow-headers': 'X-Auth-Key, X-Auth-Email, Content-Type',
@@ -38,18 +40,14 @@ const proxy = http.createServer( function(request, response){
         proxy_response.pipe(response);
         proxy_response.headers['access-control-allow-origin'] = '*';
 
-        proxy_response.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-
         response.writeHead(proxy_response.statusCode, proxy_response.headers);
     });
 
     request.pipe(proxy_request);
 });
 
-proxy.listen(8010, '127.0.0.1', function(){
+proxy.listen(PROXY_PORT, PROXY_ADDRESS, function(){
     const address = proxy.address().address;
     const port = proxy.address().port;
-    console.log(`Server avviato all'indirizzo http://${address}:${port}`);    
+    console.log(`Proxy started at http://${address}:${port}`);    
 });
