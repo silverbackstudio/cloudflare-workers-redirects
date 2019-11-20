b6b7bcbed617bc02476c0381f893a9988866d1da
Brando Meniconi <b.meniconi@silverbackstudio.it>
2019-11-20 18:02:55 +0100

    use http-proxy package to handle cfApiProxy

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,8 @@
 const functions = require('firebase-functions');
-const https = require('https');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer(); 
 
-exports.cfwRedirectsProxy = functions.https.onRequest((request, response) => {
-    console.log( '%s %s', request.method, request.url);
-
-    if ( request.method === 'OPTIONS' ) {
-        response.writeHead(200, { 
-            'access-control-allow-origin': '*',
-            'access-control-allow-headers': 'X-Auth-Key, X-Auth-Email, Content-Type',
-            'access-control-allow-methods': 'GET, POST, PUT, DELETE'
-        });
-        response.end();
-        return;
-    }
-
-    var request_options = {
-        host: 'api.cloudflare.com',
-        port: 443,
-        path: request.url,
-        method: request.method,
-        headers: { 
-            'Content-Type': 'application/json',
-        }
-    };
-
-    if ( request.headers['x-auth-key'] ) {
-        request_options.headers['x-auth-key'] = request.headers['x-auth-key'];
-    }
-
-    if ( request.headers['x-auth-email'] ) {
-        request_options.headers['x-auth-email'] = request.headers['x-auth-email'];
-    }    
-
-    var proxy_request = https.request(request_options, function(proxy_response){
-        proxy_response.pipe(response);
-        proxy_response.headers['access-control-allow-origin'] = '*';
-
-        response.writeHead(proxy_response.statusCode, proxy_response.headers);
-    });
-
-    request.pipe(proxy_request);
+exports.cloudflareApiProxy = functions.https.onRequest((req, res) => {
+    console.log( 'REQUEST: %s %s', req.method, res.url);
+    proxy.web(req, res, { target: 'https://api.cloudflare.com', changeOrigin: true });
 });
